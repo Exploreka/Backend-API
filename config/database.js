054c4925c45edebc5c4333682ff1a94a9d3ec2f7
Hildan Mulyo <hildanmulyo@std.unissula.ac.id>
2023-05-23 22:34:35 +0700

    .env database pg
    ini revisi .env buat database PG

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 dotenv.config()
 
 const db = new pg.Pool ({
-    user: 'me',
-    host: 'localhost',
-    database: 'api',
-    password: 'password',
-    port: 5432,
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
 });
 
 export default db;

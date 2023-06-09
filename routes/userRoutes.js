078224194a6416fcf83df4c00534ef390aaa9303
sendhyrama <sendhyrama@gmail.com>
2023-06-09 10:55:40 +0700

    fix: import module

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const userController = require('../controllers/userController');
-const { verifyToken } = require('../middlewares/userAuth')
+const { verifyToken } = require('../Middlewares/userAuth')
 
 const router = express.Router()
 
